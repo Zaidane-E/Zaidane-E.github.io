33fb6e13a67b3a65d9523bb5c620132b2ac68188
Zaidane-E <elhaouarizaidane@gmail.com>
2021-05-31 08:53:13 +0100

    Proofreading

--- lab02/scripts/groceries.js
+++ lab02/scripts/groceries.js
@@ -6,9 +6,9 @@
 
 var products = [
 	{
-		name: "brocoli - $1.99",
+		name: "broccoli - $1.99",
 		vegetarian: true,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: true,
 		price: 1.99
@@ -16,7 +16,7 @@ var products = [
 	{
 		name: "bread - $2.35",
 		vegetarian: true,
-		glutenFree: false,
+		lactoseFree: false,
 		nutFree: true,
 		organic: true,
 		price: 2.35
@@ -24,7 +24,7 @@ var products = [
 	{
 		name: "chicken - $14.00",
 		vegetarian: false,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: true,
 		price: 14.00
@@ -32,7 +32,7 @@ var products = [
 	{
 		name: "salmon - $10.00",
 		vegetarian: false,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: true,
 		price: 10.00
@@ -40,23 +40,23 @@ var products = [
 	{
 		name: "peanut butter - $3.50",
 		vegetarian: true,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: false,
 		organic: false,
 		price: 3.50
 	},
 	{
-		name: "eggs - $8.00",
-		vegetarian: false,
-		glutenFree: true,
+		name: "eggs - $3.00",
+		vegetarian: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: true,
-		price: 8.00
+		price: 3.00
 	},
 	{
 		name: "milk - $4.00",
 		vegetarian: false,
-		glutenFree: false,
+		lactoseFree: false,
 		nutFree: true,
 		organic: true,
 		price: 4.00
@@ -64,7 +64,7 @@ var products = [
 	{
 		name: "snickers - $2.00",
 		vegetarian: true,
-		glutenFree: false,
+		lactoseFree: false,
 		nutFree: false,
 		organic: false,
 		price: 2.00
@@ -72,7 +72,7 @@ var products = [
 	{
 		name: "banana - $1.30",
 		vegetarian: true,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: true,
 		price: 1.30
@@ -80,7 +80,7 @@ var products = [
 	{
 		name: "tuna - $1.00",
 		vegetarian: false,
-		glutenFree: true,
+		lactoseFree: true,
 		nutFree: true,
 		organic: false,
 		price: 1.00
@@ -98,7 +98,7 @@ function restrictListProducts(prods, restriction) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
 			product_names.push(prods[i].name);
 		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+		else if ((restriction == "lactoseFree") && (prods[i].lactoseFree == true)){
 			product_names.push(prods[i].name);
 		}
 		else if (restriction == "NutFree"){
@@ -112,6 +112,53 @@ function restrictListProducts(prods, restriction) {
 		}
 	}
 	return product_names;
+}
+
+function restrictProducts() {
+
+	console.log(products);
+
+	var vegetarianCheck = document.getElementById("Vegetarian").checked;
+	var lactoseFreeCheck = document.getElementById("lactoseFree").checked;
+	var nutFreeCheck = document.getElementById("NutFree").checked;
+	var organicCheck = document.getElementById("Organic").checked;
+
+	restrictedList = products;
+
+	if ( vegetarianCheck ) {
+		var restrictedList = restrictedList.filter(function (el) {
+			return el.vegetarian === true;
+		});
+	}
+
+	if ( lactoseFreeCheck ) {
+		var restrictedList = restrictedList.filter(function (el) {
+			return el.lactoseFree === true;
+		});
+	}
+
+	if ( nutFreeCheck ) {
+		var restrictedList = restrictedList.filter(function (el) {
+			return el.nutFree === true;
+		});
+	}
+
+	if ( organicCheck ) {
+		var restrictedList = restrictedList.filter(function (el) {
+			return el.organic === true;
+		});
+	}
+
+	console.log(restrictedList);
+
+	let product_names = [];
+
+	for (let i=0; i<restrictedList.length; i+=1) {
+		product_names.push(restrictedList[i].name);
+	}
+
+	return product_names;
+
 }
 
 // Calculate the total price of items, with received parameter being a list of products
--- lab02/scripts/main.js
+++ lab02/scripts/main.js
@@ -1,5 +1,5 @@
-// CODE INSPIRÉ DE:
-// https://carolinebarriere.github.io/SEG3125-Module2-Grocery/
+	// CODE INSPIRÉ DE:
+	// https://carolinebarriere.github.io/SEG3125-Module2-Grocery/
 
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
@@ -28,6 +28,7 @@ function openInfo(evt, tabName) {
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
+/*
 function populateListProductChoices(slct1, slct2) {
     var s1 = document.getElementById(slct1);
     var s2 = document.getElementById(slct2);
@@ -36,7 +37,7 @@ function populateListProductChoices(slct1, slct2) {
     s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var optionArray = restrictProducts();
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
@@ -63,7 +64,45 @@ function populateListProductChoices(slct1, slct2) {
 		s2.appendChild(document.createElement("br"));    
 	}
 }
+*/
+
+function populateProductList(slct2) {
+    var s2 = document.getElementById(slct2);
 	
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+    s2.innerHTML = "";
+		
+	// obtain a reduced list of products based on restrictions
+    var optionArray = restrictProducts();
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+		
+	for (i = 0; i < optionArray.length; i++) {
+			
+		var productName = optionArray[i];
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		checkbox.id = productName;
+		s2.appendChild(checkbox);
+		
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName));
+		s2.appendChild(label);
+		
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));    
+	}
+}
+
+
+
 // This function is called when the "Add selected items to cart" button in clicked
 // The purpose is to build the HTML to be displayed (a Paragraph) 
 // We build a paragraph to contain the list of selected items, and the total price
@@ -100,5 +139,13 @@ function selectedItems(){
 
 // ADDED CODE
 
+function openProduct() {
+	document.getElementById("product-open").click();
+}
+
+function openCart() {
+	document.getElementById("cart-open").click();
+}
+
 document.getElementById("default-open").click();
-populateListProductChoices("dietSelect", "displayProduct")
+populateProductList("displayProduct");
